eb447e9c5629999642b1525aa6f44ab39d881b37
mrunals01 <mrunalsambrekar.01@gmail.com>
2015-08-24 11:03:24 +0530

    This is new program

--- share.cpp
+++ share.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 int main()
 {
-cout<<"c++ program";
-cout<<" perform the addition of number";
+  int a=10, b=20, c;
+  c = a + b;
+  cout<<"c++ program";
+  cout<<"Addition of numbers is: "<<c;
 }
